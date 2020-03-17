c3df546410fedf1bd62c123d161101e19d1113f2
krapnik <krapnik@qq.com>
2020-03-17 13:07:56 +0800

    clear code

--- src/demo/ScratchCard.ts
+++ src/demo/ScratchCard.ts
@@ -1,7 +1,5 @@
 export default class ScratchCard {
     private _view: fgui.GComponent;
-    private _bagWindow: fgui.Window;
-
     constructor() {
         fgui.UIPackage.loadPackage("res/UI/ScratchCard", Laya.Handler.create(this, this.onUILoaded));
     }
@@ -10,9 +8,6 @@ export default class ScratchCard {
         this._view = fgui.UIPackage.createObject("ScratchCard", "Main").asCom;
         this._view.makeFullScreen();
         fgui.GRoot.inst.addChild(this._view);
-
-        // this._bagWindow = new BagWindow();
-        // this._view.getChild("bagBtn").onClick(this, () => { this._bagWindow.show(); });
     }
 
     destroy() {
