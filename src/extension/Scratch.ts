2b3d15bc4097fda69865055f1c228f18ceedb79e
krapnik <krapnik@qq.com>
2020-03-17 12:17:32 +0800

    增加 布点检测

--- src/extension/Scratch.ts
+++ src/extension/Scratch.ts
@@ -4,6 +4,8 @@ export default class Scratch extends fgui.GComponent {
     private preX: number = 0;
     private preY: number = 0;
     private firstBrush: boolean = true;
+    private _circleDiameter:number = 40;
+    private _percentCover:number = 0.5;
     protected constructFromXML(xml: any): void {
         super.constructFromXML(xml);
         this.on(Laya.Event.MOUSE_DOWN, this, this.onMouseDown);
@@ -20,27 +22,62 @@ export default class Scratch extends fgui.GComponent {
             if (this.firstBrush) {
                 this._maskPanel.displayObject.graphics.clear();//清除遮罩的位图数据
                 this.firstBrush = false;
+                this.createPoints(this._maskPanel.width, this._maskPanel.height);
             }
             this.preX = evt.target.mouseX;
             this.preY = evt.target.mouseY;
-            this._maskPanel.displayObject.graphics.drawCircle(this.preX, this.preY, 40, "#a5ff00");//绘图填充位图内容，补充遮罩
+            this._maskPanel.displayObject.graphics.drawCircle(this.preX, this.preY, this._circleDiameter, "#a5ff00");//绘图填充位图内容，补充遮罩
             this.on(Laya.Event.MOUSE_MOVE, this, this.onMouseMove);
         }
         if (this._panel) {//刮刮卡的时候，补位图层隐藏
             this._panel.visible = false;
         }
     }
+
+    
     private onMouseMove(evt: any): void {
         var curX = evt.target.mouseX;
         var curY = evt.target.mouseY;
         if (this._maskPanel) {
-            this._maskPanel.displayObject.graphics.drawCircle(curX, curY, 40, "#a5ff00");
+            this._maskPanel.displayObject.graphics.drawCircle(curX, curY, this._circleDiameter, "#a5ff00");
             this.preX = curX;
             this.preY = curY
+        }
+        var point = { x:curX, y:curY} as laya.maths.Point;
+        this.pointsHitTest(this._pointsArr,point);
+        console.log(`${this._pointsArr.length}/${this._pointCount}`);
+        if(this._pointsArr.length/this._pointCount<=this._percentCover){
+            this._maskPanel.displayObject.graphics.drawRect(0,0,this._maskPanel.width, this._maskPanel.height,"#000");
         }
     }
     private onMouseUp(evt: any): void {
         this.off(Laya.Event.MOUSE_MOVE, this, this.onMouseMove);
     }
 
+    private _pointCount: number = 100;
+    private _row: number = 10;
+    private _col: number = 10;
+    private _pointsArr: Array<Laya.Point> = [];
+    private createPoints(width: number, height: number) {
+        let widthSpan: number = Math.trunc(width / this._row),
+            heightSpan: number = Math.trunc(height / this._col);
+        for (let i = 0; i < this._row; i++) {
+            for (let j = 0; j < this._col; j++) {
+                let point = new Laya.Point(widthSpan * i, heightSpan * j);
+                this._pointsArr.push(point);
+            }
+        }
+    }
+
+    private pointsHitTest(points:Array<Laya.Point>,source:laya.maths.Point):boolean {
+        return points.some((point:Laya.Point,index:number)=>{
+            if(point.distance(source.x,source.y)<=this._circleDiameter){
+                points.splice(index,1);
+                return true;
+            }else{
+                return false;
+            }
+        })
+    }
+    
 }
