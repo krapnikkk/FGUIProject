4589a8953c42f04c3f8de684e7cfb534ab666e1e
krapnik <krapnik@qq.com>
2020-03-30 22:46:55 +0800

    [modified] datepicker update

--- src/demo/DatePickerDemo.ts
+++ src/demo/DatePickerDemo.ts
@@ -1,6 +1,7 @@
 export default class DatePickerDemo {
     private _view: fgui.GComponent;
     private _btn: fgui.GButton;
+    private _dateText: fgui.GTextField;
     constructor() {
         fgui.UIPackage.loadPackage("res/UI/DatePicker", Laya.Handler.create(this, this.onUILoaded));
     }
@@ -10,15 +11,23 @@ export default class DatePickerDemo {
         this._view.makeFullScreen();
         fgui.GRoot.inst.addChild(this._view);
         this._btn = this._view.getChild("n1").asButton;
-        this._btn.onClick(this,this.show);
+        this._btn.onClick(this, this.show);
+
+        this._dateText = this._view.getChild("n2").asTextField;
+        Laya.stage.on("set_date", this, this.updateDate);
+        Laya.stage.event("dataPicker_init");
     }
 
-    show():void {
+    show(): void {
         Laya.stage.event("date_show");
     }
 
+    updateDate(year: number, month: number, day: number): void {
+        this._dateText.setVar("year", year + "").setVar("month", month + "").setVar("date", day + "").flushVars();
+    }
+
     destroy() {
-        this._btn.offClick(this,this.show);
+        this._btn.offClick(this, this.show);
         fgui.UIPackage.removePackage("DatePicker");
     }
 }
--- src/extension/DatePicker.ts
+++ src/extension/DatePicker.ts
@@ -2,19 +2,23 @@ export default class DatePicker extends fgui.GComponent {
     private _btnCancel: fgui.GButton;
     private _btnComfirm: fgui.GButton;
     private _yearList: fgui.GList;
-    private _yearCount: number = 100;
-    private _startYear: number = 1921;
+    private _yearTotalCount: number = new Date().getFullYear();
+    private _yearCount: number = 101;
+    private _startYear: number = this._yearTotalCount - this._yearCount;
     private _monthList: fgui.GList;
-    private _monthCount: number = 12;
+    private _monthTotalCount: number = 12;
     private _startMonth: number = 1;
     private _dateList: fgui.GList;
-    private _dateCount: number = 31;
+    private _dateTotalCount: number = 31;
     private _startDate: number = 1;
-    private _listRow:number = 3;
+    private _listRow: number = 4;
+    private _currentYear: number;
+    private _currentMonth: number;
+    private _currentDate: number;
 
     protected constructFromXML(xml: any): void {
         super.constructFromXML(xml);
-        this.init();
+        Laya.stage.on("dataPicker_init", this, this.init);
     }
 
 
@@ -29,27 +33,32 @@ export default class DatePicker extends fgui.GComponent {
         this._btnCancel.onClick(this, this.hide);
         this._btnComfirm = this.getChild("n9").asButton;
         this._btnComfirm.onClick(this, this.hide);
+
         this._yearList = this.getChild("year_list").asList;
         this._yearList.setVirtualAndLoop();
         this._yearList.itemRenderer = Laya.Handler.create(this, this.renderYearListItem, null, false);
         this._yearList.numItems = this._yearCount;
+        this._yearList.on(fgui.Events.SCROLL, this, this.yearOnScroll);
 
         this._monthList = this.getChild("month_list").asList;
         this._monthList.setVirtualAndLoop();
         this._monthList.itemRenderer = Laya.Handler.create(this, this.renderMonthListItem, null, false);
-        this._monthList.numItems = this._monthCount;
+        this._monthList.numItems = this._monthTotalCount;
+        this._monthList.on(fgui.Events.SCROLL, this, this.monthOnScroll);
 
         this._dateList = this.getChild("date_list").asList;
         this._dateList.setVirtualAndLoop();
         this._dateList.itemRenderer = Laya.Handler.create(this, this.renderDateListItem, null, false);
-        this._dateList.numItems = this._dateCount;
+        this._dateList.numItems = this._dateTotalCount;
+        this._dateList.on(fgui.Events.SCROLL, this, this.dateOnScroll);
+
         let date = new Date();
-        this.setDate(date.getFullYear(), date.getMonth(), date.getDate());
+        this.setDate(date.getFullYear(), date.getMonth() + 1, date.getDate());
     }
 
     public renderYearListItem(index: number, obj: fgui.GObject): void {
         var item: fgui.GButton = <fgui.GButton>obj;
-        item.title = this._startYear + index + "年";
+        item.title = this._startYear + index + 1 + "年";
     }
 
     public renderMonthListItem(index: number, obj: fgui.GObject): void {
@@ -62,10 +71,66 @@ export default class DatePicker extends fgui.GComponent {
         item.title = this._startDate + index + "日";
     }
 
-    public setDate(year: number, month: number, day: number) {
-        this._yearList.scrollToView(3);
-        this._monthList.scrollToView(2);
-        this._dateList.scrollToView(1);
+    yearOnScroll() {
+        let nowIndex = this._yearList.getFirstChildInView();
+        this._currentYear = nowIndex + this._listRow > this._yearCount ? this._startYear + nowIndex + this._listRow - this._yearCount : this._startYear + nowIndex + this._listRow;
+        this.updateDateList();
+        this.updateDateText();
+    }
+
+    monthOnScroll() {
+        let nowIndex = this._monthList.getFirstChildInView();
+        this._currentMonth = nowIndex + this._listRow > this._monthTotalCount ? this._listRow + nowIndex - this._monthTotalCount : this._listRow + nowIndex;
+        this.updateDateList();
+        this.updateDateText();
+    }
+
+    dateOnScroll() {
+        let nowIndex = this._dateList.getFirstChildInView();
+        this._currentDate = nowIndex + this._listRow > this._dateTotalCount ? this._listRow + nowIndex - this._dateTotalCount : this._listRow + nowIndex;
+        this.updateDateText();
+    }
+
+    public setDate(year: number, month: number, date: number) {
+        year -= this._startYear;
+        let yearIdx = year - this._listRow >= 0 ? year - this._listRow : this._yearTotalCount + year - this._listRow,
+            monthIdx = month - this._listRow >= 0 ? month - this._listRow : this._monthTotalCount + month - this._listRow,
+            dateIdx = date - this._listRow >= 0 ? date - this._listRow : this._dateTotalCount + date - this._listRow;
+        this._yearList.scrollToView(yearIdx);
+        this._monthList.scrollToView(monthIdx);
+        this._dateList.scrollToView(dateIdx);
+    }
+
+    updateDateList() {
+        if (this._currentMonth == 2) {
+            if (this._currentYear % 4 == 0) {
+                this._dateList.numItems = 29;
+                this._dateTotalCount = 29;
+            } else {
+                this._dateList.numItems = 28;
+                this._dateTotalCount = 28;
+            }
+        } else if (this._currentMonth == 1 || this._currentMonth == 3 || this._currentMonth == 5 || this._currentMonth == 7 || this._currentMonth == 10 || this._currentMonth == 12) {
+            this._dateList.numItems = 31;
+            this._dateTotalCount = 31;
+        } else {
+            this._dateList.numItems = 30;
+            this._dateTotalCount = 30;
+        }
+        if (this._currentDate > 0) {
+            if (this._currentDate > this._dateList.numItems) {
+                this._dateList.scrollToView(this._dateTotalCount - this._listRow + 1);//复位到1日
+            } else {
+                let dateIdx = this._currentDate - this._listRow >= 0 ? this._currentDate - this._listRow : this._dateTotalCount + this._currentDate - this._listRow;
+                this._dateList.scrollToView(dateIdx);
+            }
+            this.dateOnScroll();
+        }
+
+    }
+
+    updateDateText() {
+        Laya.stage.event("set_date", [this._currentYear, this._currentMonth, this._currentDate]);
     }
 
     public show(): void {
